9da0b52f9bb0ce79fc34fc87269cd7eff627b5e5
diogo596 <diogo596@hotmail.fr>
2023-11-29 10:21:28 +0100

    express-Quets done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const app = require("./src/app");
+const app = require('./src/app');
+require('dotenv').config();
 
-const port = 5000;
+const port = 5500;
 
 app
   .listen(port, () => {
     console.log(`Server is listening on ${port}`);
   })
-  .on("error", (err) => {
-    console.error("Error:", err.message);
+  .on('error', (err) => {
+    console.error('Error:', err.message);
   });
