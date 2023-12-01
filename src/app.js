878b946a7b96b1b48dd19ac79ec0bdfed211de35
diogo596 <diogo596@hotmail.fr>
2023-12-01 10:02:49 +0100

    methode post done

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const movieControllers = require('./controllers/movieControllers');
 const usersControllers = require('./controllers/usersControllers');
 
+app.post('/api/movies', movieControllers.postMovie);
+app.post('/api/users/:id', usersControllers.postUser);
 app.get('/api/movies', movieControllers.getMovies);
 app.get('/api/movies/:id', movieControllers.getMovieById);
 app.get('/api/users', usersControllers.getUsers);
