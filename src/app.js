85eddce417e5e0c67ae013062e8d7a0161699d28
diogo596 <diogo596@hotmail.fr>
2023-12-01 11:36:45 +0100

    PUT methode done

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.use(express.json());
 const movieControllers = require('./controllers/movieControllers');
 const usersControllers = require('./controllers/usersControllers');
 
+app.put('/api/movies/:id', movieControllers.updateMovie);
 app.post('/api/movies', movieControllers.postMovie);
 app.post('/api/users/:id', usersControllers.postUser);
 app.get('/api/movies', movieControllers.getMovies);
